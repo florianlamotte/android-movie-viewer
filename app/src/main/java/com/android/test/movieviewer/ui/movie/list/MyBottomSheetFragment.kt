bb7d36d1a96996c0d47df3727ce1c3261ba36dd6
Florian <florian.lamotte@gmail.com>
2021-09-08 18:31:08 +0100

    Added max height

--- app/src/main/java/com/android/test/movieviewer/ui/movie/list/MyBottomSheetFragment.kt
+++ app/src/main/java/com/android/test/movieviewer/ui/movie/list/MyBottomSheetFragment.kt
@@ -1,12 +1,18 @@
 package com.android.test.movieviewer.ui.movie.list
 
+import android.app.Activity
+import android.app.Dialog
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.android.test.movieviewer.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.PEEK_HEIGHT_AUTO
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
 
 class MyBottomSheetFragment: BottomSheetDialogFragment() {
 
@@ -14,7 +20,7 @@ class MyBottomSheetFragment: BottomSheetDialogFragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         return inflater.inflate(
             R.layout.bottomsheet_fragment,
             container,
@@ -29,6 +35,30 @@ class MyBottomSheetFragment: BottomSheetDialogFragment() {
             replace(R.id.bottomsheet_fragment_container, fragmentList)
                 .commit()
         }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState).apply {
+            (this as? BottomSheetDialog)
+                ?.behavior
+                ?.apply {
+                    setPeekHeight(PEEK_HEIGHT_AUTO, true)
+                    isFitToContents = false
+                    expandedOffset = getBottomSheetDialogDefaultHeight()
+                    state = BottomSheetBehavior.STATE_EXPANDED
+                }
+        }
+    }
+
+    private fun getBottomSheetDialogDefaultHeight(): Int {
+        return getWindowHeight() * 15 / 100 // 15%
+    }
+
+    private fun getWindowHeight(): Int {
+        // Calculate window height for fullscreen use
+        val displayMetrics = DisplayMetrics()
+        (context as Activity?)!!.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics.heightPixels
     }
 
     companion object {
